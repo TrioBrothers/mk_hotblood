160f2b8fa2c6cecc48ee21f24c05b3d6bbaf33a7
minkyuChung <mink91j@gmail.com>
2021-01-20 10:33:11 +0900

    불필요한 동적할당 제거 - Account 클래스 멤버변수 name 정적할당 선언

--- Project1/Account.h
+++ Project1/Account.h
@@ -2,18 +2,18 @@
 #define __ACCOUNT_H_
 #include <cstring>
 #include <iostream>
+#define MAX_LEN 30
 using namespace std;
 
 class Account {
 private:
-	char* name;			//회원명
-	int id;				//회원ID
-	int balance;		//잔액
+	char name[MAX_LEN];			//회원명
+	int id;						//회원ID
+	int balance;				//잔액
 public:
 	Account(char* name, int id, int bal)
 		:balance(bal), id(id)
 	{
-		this->name = new char[strlen(name) + 1];
 		strcpy(this->name, name);
 	}
 	Account(const Account& copy)					//깊은 복사생성자
